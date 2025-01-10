ddc35ef5dff39930b3dfa9e872132f384b2f480c
Alexander Busch <alexander.busch@alumni.ntnu.no>
2025-01-10 10:13:42 +0100

    Create ZoteroProfileSelector.bat

---
+++ Zotero/ZoteroProfileSelector.bat
@@ -0,0 +1,8 @@
+@echo off
+
+rem Opens Zotero profile selector
+rem https://www.zotero.org/support/kb/multiple_profiles
+rem https://forums.zotero.org/discussion/69465/is-there-a-way-to-quickly-switch-between-zotero-5-profile-folders
+
+cd "C:\Program Files (x86)\Zotero\"
+zotero.exe -P -no-remote
